299dd596e63ee84cda1edd1ed2621b646b4ec12c
Gabriel <camilo1234299@gmail.com>
2024-04-17 15:14:13 -0500

    finalaizado cuenta CDT

--- src/ufps/poo2/ejercicio/banco/CDTAccount.java
+++ src/ufps/poo2/ejercicio/banco/CDTAccount.java
@@ -4,13 +4,13 @@ import javax.management.RuntimeErrorException;
 
 public class CDTAccount extends Account {
 
-    private double monto = this.getBalance();
+    private double monto;
     private int meses;
 
     public CDTAccount(int numeroDeCuneta, double monto, int meses) {
         super(numeroDeCuneta);
         if (meses <= 0)
-            throw new RuntimeException("Los mese deben de ser mayor que cero");
+            throw new RuntimeException("Los meses deben de ser mayor que cero");
         this.monto = monto;
         this.meses = meses;
     }
@@ -22,8 +22,8 @@ public class CDTAccount extends Account {
     @Override
     public void deposit(double monto) {
         System.out.println(monto);
-        if (monto == 0) {
-            deposit(monto);
+        if (this.monto == 0) {
+            this.monto = monto;
         } else {
             throw new RuntimeException("No puede depositar");
         }
@@ -31,7 +31,7 @@ public class CDTAccount extends Account {
 
     @Override
     public void withdraw(double interes) {
-        for (int i = 0; i <= meses; i++) {
+        for (int i = 0; i < meses; i++) {
             monto += (((monto * interes) / 100));
         }
         System.out.println(monto);
